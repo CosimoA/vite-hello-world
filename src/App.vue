20854671d6e4d87c75493433e15b7ace1b0e11d5
CosimoA <cosimo.archip@gmail.com>
2023-12-20 18:25:36 +0100

    FEAT: change Title & add Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  components: {
+    AppTitle
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/QCbh.gif" class="logo vue" alt="Vue logo" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle />
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 20em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
